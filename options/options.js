cdf4e6d1d93db241761c7e2c5bb74f00a9e5c0e1
Crowdbotics <team@crowdbotics.com>
2021-08-07 00:21:11 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "throbbing_wildflowe_29590Identifier",
+  url: "https://throbbing_wildflowe_29590Identifier.botics.co",
+  api: "https://throbbing_wildflowe_29590Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
